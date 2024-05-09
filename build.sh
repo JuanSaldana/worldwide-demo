d42e4c828254b772d03cb08aefa96a906f520c8a
Juanantonio-Saldana_ghub <Juanantonio.Saldana@blueyonder.com>
2024-05-09 13:04:56 -0600

    fix: faulty command line fixed

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-python createsuperuser --noinput
+python manage.py createsuperuser --noinput
