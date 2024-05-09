fb18d151d48647b8c8c834ae887bf378c2dcc108
Juanantonio-Saldana_ghub <Juanantonio.Saldana@blueyonder.com>
2024-05-09 13:29:33 -0600

    models updated

--- dashboard_app/models.py
+++ dashboard_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Song(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Artist(models.Model):
+    artistic_name = models.CharField(max_length=50)
+    songs = models.ManyToManyField(Song)
